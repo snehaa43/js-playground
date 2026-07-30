35b5ae3256b8f172e1e6ac9b7a7494acc3b1d8d6
snehaa43 <snehak2929@gmail.com>
2026-07-30 19:16:30 +0530

    Remove unrelated projects

--- practice/my-app/src/components/badge.jsx
+++
@@ -1,9 +0,0 @@
-function badge({text,bgcolour}){
-    return(
-        <span className="badge" style={{ backgroundColor: bgcolour }}>
-            {text}
-        </span>
-    );  
-
-}
-export default badge;
